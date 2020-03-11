20fb7725bfb0cfe8937c0662fb7437fa67b70c0c
Behemoth86 <megadeth86@mail.ru>
2020-03-12 00:43:42 +0400

    feat: add function countCats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,16 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function countCats(matrix) {
+  let sum = 0;
+  if (matrix.length === 0) {
+    return sum;
+  }
+  else{
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        if (matrix[i][j] == '^^') {
+           sum= sum + 1;
+        }
+      }
+    }
+    return sum;
+  }
+}
