2c7ab1df4a485c3d3aa384e682772a03a8f33ca2
Behemoth86 <megadeth86@mail.ru>
2020-03-21 19:41:50 +0400

    fix: fix function getSeason

--- src/what-season.js
+++ src/what-season.js
@@ -1,14 +1,8 @@
 module.exports = function getSeason(date){
-  if (date === undefined) {
-    return "Unable to determine the time of year!";
-  }
-  else if ((date instanceof Date === false) || (typeof date !== "object")){
-    throw new Error("Error");
-  }
-  else{
-    if (((date.getMonth()+1)==1) || (date.getMonth()+1==2) || (date.getMonth()==12)) return "winter"
-    else if (3<=(date.getMonth()+1)<=6) return "spring"
-    else if (5<=(date.getMonth()+1)<=9) return "summer"
-    else if (8<=(date.getMonth()+1)<=12) return "autumn";
-  }
+  if (!date) { return 'Unable to determine the time of year!'; }
+  try { date.getTime(); } catch (e) { throw new Error(e); }
+  if (((date.getMonth()+1)==1) || (date.getMonth()+1==2) || ((date.getMonth()+1)==12)) return "winter"
+  else if (((date.getMonth()+1)>=3) && ((date.getMonth()+1)<=5)) return "spring"
+  else if (((date.getMonth()+1)>=6) && ((date.getMonth()+1)<=8)) return "summer"
+  else if (((date.getMonth()+1)>=9) && ((date.getMonth()+1)<=11)) return "autumn";
 };
