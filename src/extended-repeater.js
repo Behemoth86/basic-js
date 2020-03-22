18591812a0ec4bc9b08693cea3147fe76461c1ec
Behemoth86 <megadeth86@mail.ru>
2020-03-22 21:15:32 +0400

    feat: function repeater extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,10 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let resultString ='';
+  let {repeatTimes=1, separator='+', addition='', additionRepeatTimes=1, additionSeparator='|'} = options;
+  let addseparator = addition + additionSeparator;
+  let addrepeat = addseparator.repeat(additionRepeatTimes - 1) + addition;
+  let newStr = str + addrepeat + separator;
+  resultString = newStr.repeat(repeatTimes - 1) + str + addrepeat;
+  return resultString;
 };
   
